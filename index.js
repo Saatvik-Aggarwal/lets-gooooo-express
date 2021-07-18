a7c4c73c5ec447825222a53a040c119376644e58
Mystical-Codez <37031914+Saatvik-Aggarwal@users.noreply.github.com>
2021-07-17 21:58:15 -0700

    moved functions to main file

--- index.js
+++ index.js
@@ -15,8 +15,52 @@ app.use(
 )
 
 app.get('/users', db.getUsers)
-app.post('/signup', db.signUp)
-app.post('/login', db.login)
+
+app.post('/signup', (request, response) => {
+    let username = request.body.username
+    let password = request.body.password
+    let email = request.body.email
+
+    bcrypt.hash(password, 10, (err, hash) => {
+        pool.query(`INSERT INTO users (username, password, email) VALUES ('${username}', '${hash}', '${email}')` + usern, (error, results) => {
+            if (error) {
+                response.json({message: "Failure. Duplicate username or email detected!"})
+            } else {
+                response.status(200).json({message: "Success"})
+            }
+            
+        })
+    })
+
+})
+
+app.post('/login', (request, response) => {
+    let username = request.body.username
+    let password = request.body.password
+
+    
+    pool.query(`SELECT password FROM users WHERE username='${username}'` + usern, (error, results) => {
+        if (error) {
+            response.json({message: "Server side failure!"})
+        } else {
+            results.rows.forEach(row => {
+                bcrypt.compare(password, row['password'], function(error, match) {
+                    if (match) {
+                        response.status(200).json({message: "Success"})
+                        return
+                    }
+                    // response == true if they match
+                    // response == false if password is wrong
+                });
+            });
+            response.status(200).json({message: "Incorrect username or password."});
+        }
+        
+    })
+    
+
+})
+
 app.get('/', (request, response) => {
     response.json({info: 'HELLO BOOOMER'})
 })
--- server.js
+++ server.js
@@ -22,50 +22,9 @@ const getUsers = (request, response) => {
     })
 }
 
-const signUp = (request, response) => {
-    let username = request.body.username
-    let password = request.body.password
-    let email = request.body.email
+const signUp;
 
-    bcrypt.hash(password, 10, (err, hash) => {
-        pool.query(`INSERT INTO users (username, password, email) VALUES ('${username}', '${hash}', '${email}')` + usern, (error, results) => {
-            if (error) {
-                response.json({message: "Failure. Duplicate username or email detected!"})
-            } else {
-                response.status(200).json({message: "Success"})
-            }
-            
-        })
-    })
-
-}
-
-const login = (request, response) => {
-    let username = request.body.username
-    let password = request.body.password
-
-    
-    pool.query(`SELECT password FROM users WHERE username='${username}'` + usern, (error, results) => {
-        if (error) {
-            response.json({message: "Server side failure!"})
-        } else {
-            results.rows.forEach(row => {
-                bcrypt.compare(password, row['password'], function(error, match) {
-                    if (match) {
-                        response.status(200).json({message: "Success"})
-                        return
-                    }
-                    // response == true if they match
-                    // response == false if password is wrong
-                });
-            });
-            response.status(200).json({message: "Incorrect username or password."});
-        }
-        
-    })
-    
-
-}
+const login;
 
 module.exports = {
     getUsers,
